42a8073a12caa3ef5100bb329c04b9f4525a4a5e
Graycemuthui <graycemuthui20@gmail.com>
2022-06-02 22:29:11 +0300

    Setup webpack

--- webpack.config.js
+++ webpack.config.js
@@ -8,11 +8,11 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Output Management",
+      template: "./src/index.html",
     }),
   ],
   output: {
-    filename: "[name].bundle.js",
+    filename: "[name].js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
