09b113b7cf45d9bdc0ca9b61a4444f37bc019d9f
Graycemuthui <graycemuthui20@gmail.com>
2022-06-02 22:47:32 +0300

    Create the tasks dynamically

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-let g = 1;
+import "./styles.css";
+import { items } from "./modules/tasks";
+
+const listItems = document.getElementById("todo-items");
+const displayList = () => {
+  for (let i = 0; i < items.length; i += 1) {
+    const loop = listItems.innerHTML;
+    let boxes = "";
+    if (items[i].completed === true) {
+      boxes = "checked";
+    }
+    listItems.innerHTML = `
+               <li>
+            <div class="list-wrap">
+              <div><input type="checkbox" ${boxes}/></div>
+              <div class="text-input">
+                <input
+                  type="text"
+                  class="list-item"
+                  value="${items[i].description}"
+                />
+              </div>
+              <div class="icons">
+                <i id="trash - ${items[i].index}" class="fa-solid fa-trash hide"></i>
+                <i id="ellipsis-${items[i].index}" class="fa-solid fa-ellipsis-vertical "></i>
+              </div>
+            </div>
+          </li> ${loop}`;
+  }
+};
+
+displayList();
---
+++ src/modules/tasks.js
@@ -0,0 +1,22 @@
+export const items = [
+  {
+    index: "0",
+    completed: "true",
+    description: "Take my dog for a walk",
+  },
+  {
+    index: "1",
+    description: "Go to the gym",
+    completed: "false",
+  },
+  {
+    index: "2",
+    description: "Bake cookies",
+    completed: "true",
+  },
+  {
+    index: "3",
+    description: "Wash my hair",
+    completed: "false",
+  },
+];
